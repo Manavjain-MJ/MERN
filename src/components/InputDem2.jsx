41ee906d4b1234be3a59b88b4e0a5053a2ec28b5
Manavjain-MJ <manavjain2347@gmail.com>
2025-02-11 12:22:08 +0530

    Router

--- src/components/InputDem2.jsx
+++ src/components/InputDem2.jsx
@@ -11,19 +11,21 @@ export const InputDem2 = (e) => {
     <div style={{textAlign:"center"}}>
         <h1>InputDem2</h1>
         <label>Name</label>
-        <input type="text" placeholder='enter name' onChange={(e)=>setname(e.target.value)}></input>
-        <h3>Name: {name}</h3>
+        <input type="text" placeholder='enter name' onChange={(e)=>setname(e.target.value)}></input><br />
         <label>Email</label>
-        <input type="text" placeholder='enter email' onChange={(e)=>setemail(e.target.value)}></input>
-        <h3>Email: {email}</h3>
+        <input type="text" placeholder='enter email' onChange={(e)=>setemail(e.target.value)}></input><br />
         <label>Contact</label>
-        <input type="text" placeholder='enter contact' onChange={(e)=>setcontact(e.target.value)}></input>
-        <h3>Contact: {contact}</h3>
+        <input type="text" placeholder='enter contact' onChange={(e)=>setcontact(e.target.value)}></input><br />
         <label>Marks</label>
-        <input type="text" placeholder='enter name' onChange={(e)=>setmarks(e.target.value)}></input>
-        <h3>Marks: {marks}</h3>
+        <input type="text" placeholder='enter name' onChange={(e)=>setmarks(e.target.value)}></input><br />
         <label>Category</label>
-        <input type="text" placeholder='enter name' onChange={(e)=>setcategory(e.target.value)}></input>
+        <input type="text" placeholder='enter name' onChange={(e)=>setcategory(e.target.value)}></input><br />
+
+
+        <h3>Name: {name}</h3>
+        <h3>Email: {email}</h3>
+        <h3>Contact: {contact}</h3>
+        <h3>Marks: {marks}</h3>
         <h3>Category: {category}</h3>
 
         
